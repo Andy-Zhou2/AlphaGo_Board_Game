a73ace31508e7dae5c95cafb92c36b3ee943be2f
Andy Zhou <wz337@cam.ac.uk>
2021-12-28 15:52:01 +0000

    did testing; network tends to search 4-5 nodes extensively while neglect obvious (to human) moves However, might be okay for initial network

--- gen_play_3.py
+++ gen_play_3.py
@@ -10,15 +10,15 @@ count = 1
 
 def run_self_play(thread_id):
     print('start working on', thread_id)
-    single_game_data = generate_single_game(net, False, 800)
+    single_game_data = generate_single_game(net, True, 800)
 
     with open(f'./data/games/1_game_800_gen_0_thread_{thread_id}.pkl', 'wb') as f:
         pickle.dump(single_game_data, f)
 
 
 if __name__ == '__main__':
-    with Pool(4) as p:
-        p.map(run_self_play, range(5))
+    with Pool(1) as p:
+        p.map(run_self_play, range(1))
 
 # while True:
 #     data = []
